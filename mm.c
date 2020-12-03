cf551186567cba282ff8c1333c074b2a48b05605
Eamon Niknafs <en@csa2.bu.edu>
2020-12-03 00:50:43 -0500

    Fixed a ton of bugs - added missing undeclared global vars - fixed nonexistant constants/macros being called - uncommented code that was not supposed to be commented - added missing return statement - moved methods above where they are called

--- mm.c
+++ mm.c
@@ -32,6 +32,8 @@ team_t team = {
     "",
     ""
 };
+
+/* Constants and Macros */  
 /* header/footer size (bytes) */
 #define HFSIZE 4       
 
@@ -53,21 +55,126 @@ team_t team = {
 #define GET_ALLOC(a) (READ(a) & 0x1)
 
 /* Calculate address of given block's header/footer */
-#define HEAD(ptr) ((char *)(ptr) - WSIZE)
-#define FOOT(ptr) ((char *)(ptr) + GET_SIZE(HEAD(ptr)) - DSIZE)
+#define HEAD(ptr) ((char *)(ptr) - HFSIZE)
+#define FOOT(ptr) ((char *)(ptr) + GET_SIZE(HEAD(ptr)) - DWORD)
 
 /* Calculate address of next/previous blocks */
 #define NEXT(ptr) ((char *)(ptr) + GET_SIZE(((char *)(ptr) - HFSIZE)))
 #define PREV(ptr) ((char *)(ptr) - GET_SIZE(((char *)(ptr) - DWORD)))
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+#define ALIGN(size) (((size) + (DWORD-1)) & ~0x7)
 
+/* Global variables */  
+static char *heapL = 0; //ptr to first block
 static char *trav; //trav for next fit
 
-/************************
-// #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
-/************************
+/*
+ * coalesce - Boundary coalescing. Returns pointer to coalesced block.
+ *  - checks if next/prev blocks are allocated
+ *  - runs different cases to coalesce depending on allocation
+ */
+static void *coalesce(void *ptr) 
+{
+    size_t prev_alloc = GET_ALLOC(FOOT(PREV(ptr)));
+    size_t next_alloc = GET_ALLOC(HEAD(NEXT(ptr)));
+    size_t size = GET_SIZE(HEAD(ptr));
+
+    /* if both previous and next allocated */
+    if (prev_alloc && next_alloc) {
+        return ptr;
+    }
+
+    /* if only next is allocated */
+    else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HEAD(PREV(ptr)));
+        WRITE(FOOT(ptr), HF(size, 0));
+        WRITE(HEAD(PREV(ptr)), HF(size, 0));
+        ptr = PREV(ptr);
+    }
+
+    /* if only previous is allocated */
+    else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HEAD(NEXT(ptr)));
+        WRITE(HEAD(ptr), HF(size, 0));
+        WRITE(FOOT(ptr), HF(size,0));
+    }
+
+    /* if neither is */
+    else {
+        size += GET_SIZE(HEAD(PREV(ptr))) + 
+            GET_SIZE(FOOT(NEXT(ptr)));
+        WRITE(HEAD(PREV(ptr)), HF(size, 0));
+        WRITE(HEAD(NEXT(ptr)), HF(size, 0));
+        ptr = PREV(ptr);
+    }
+   
+    if ((trav > (char *)ptr) && (trav < NEXT(ptr))) trav = ptr; //change trav if pointing to coalesced block
+
+    return ptr;
+}
+
+/* 
+ * *grow_heap - grows the heap size.
+ *  - ensures alignment
+ *  - adds header/footer for new free block
+ *  - moves end header to the end of the grown heap
+ *  - coalesces
+ */
+static void *grow_heap(size_t size) 
+{
+    char *ptr = mem_sbrk(size); //set pointer to start of grown block
+    if ((long)ptr == -1)  return NULL;
+
+    /* Initialize new block's header/footer and end header */
+    WRITE(HEAD(ptr), HF(ALIGN(size)*HFSIZE, 0));
+    WRITE(FOOT(ptr), HF(ALIGN(size)*HFSIZE, 0));
+    WRITE(HEAD(NEXT(ptr)), HF(0, 1)); //new end header
+
+    return coalesce(ptr);
+}
+
+/* 
+ * fit - Find a fit for a block with size bytes
+ * - uses next fit method to search for a fit
+ * - first traverses the next part of the heap (not already traversed)
+ * - next, it traverses the first half
+ * - if still no fit, returns NULL, if any then returns pointer to start of fit
+ */
+static void *fit(size_t size)
+{
+    char *last_trav = trav; //next fit
+    /* search from the trav to the end of list */
+    for (; GET_SIZE(HEAD(trav)) > 0; trav = NEXT(trav)){
+        if (!GET_ALLOC(HEAD(trav)) && (size <= GET_SIZE(HEAD(trav)))) return trav;
+    }
+    /* search from start of list to last trav */
+    for (trav = heapL; trav < last_trav; trav = NEXT(trav)){
+        if (!GET_ALLOC(HEAD(trav)) && (size <= GET_SIZE(HEAD(trav)))) return trav;
+    }
+    return NULL; //no fit
+}
+
+/* 
+ * put - Puts size byte block at the free block at ptr, splitting
+ * if required.
+ */
+static void put(void *ptr, size_t size)
+{
+    size_t free_size = GET_SIZE(HEAD(ptr));   
+
+    if ((free_size - size) >= (2*DWORD)) { 
+        WRITE(HEAD(ptr), HF(size, 1));
+        WRITE(FOOT(ptr), HF(size, 1));
+        ptr = NEXT(ptr);
+        WRITE(HEAD(ptr), HF(free_size-size, 0));
+        WRITE(FOOT(ptr), HF(free_size-size, 0));
+    }
+    else { 
+        WRITE(HEAD(ptr), HF(free_size, 1));
+        WRITE(FOOT(ptr), HF(free_size, 1));
+    }
+}
 
 /* 
  * mm_init - initialize the malloc package.
@@ -101,6 +208,9 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
+    char *ptr;
+    size_t growsize;
+
     if (heapL == 0) mm_init(); //if no heap list, init
     if (size == 0) return NULL; //if request is useless, return NULL
     ptr = fit(ALIGN(size));
@@ -131,8 +241,8 @@ void mm_free(void *ptr)
     size_t size = GET_SIZE(HEAD(ptr)); //get size of block to free
     if (heapL == 0) mm_init(); //if no heap list, init
 
-    PUT(HEAD(ptr), HF(size, 0)); //set header to unallocated
-    PUT(FOOT(ptr), HF(size, 0)); //set footer to unallocated 
+    WRITE(HEAD(ptr), HF(size, 0)); //set header to unallocated
+    WRITE(FOOT(ptr), HF(size, 0)); //set footer to unallocated 
     coalesce(ptr); //coalesce to optimize
 }
 
@@ -141,125 +251,8 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
-    
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+    return 0;
 }
 
-/*
- * coalesce - Boundary coalescing. Returns pointer to coalesced block.
- *  - checks if next/prev blocks are allocated
- *  - runs different cases to coalesce depending on allocation
- */
-static void *coalesce(void *ptr) 
-{
-    size_t prev_alloc = GET_ALLOC(FOOT(PREV(ptr)));
-    size_t next_alloc = GET_ALLOC(HEAD(NEXT(ptr)));
-    size_t size = GET_SIZE(HEAD(ptr));
 
-    /* if both previous and next allocated */
-    if (prev_alloc && next_alloc) {
-        return ptr;
-    }
-
-    /* if only next is allocated */
-    else if (!prev_alloc && next_alloc) {
-        size += GET_SIZE(HEAD(PREV(ptr)));
-        PUT(FOOT(ptr), HF(size, 0));
-        PUT(HEAD(PREV(ptr)), HF(size, 0));
-        ptr = PREV(bp);
-    }
-
-    /* if only previous is allocated */
-    else if (prev_alloc && !next_alloc) {
-        size += GET_SIZE(HEAD(NEXT(ptr)));
-        PUT(HEAD(ptr), HF(size, 0));
-        PUT(FOOT(bp), HF(size,0));
-    }
-
-    /* if neither is */
-    else {
-        size += GET_SIZE(HEAD(PREV(ptr))) + 
-            GET_SIZE(FOOT(NEXT(ptr)));
-        PUT(HEAD(PREV(ptr)), HF(size, 0));
-        PUT(HEAD(NEXT(ptr)), HF(size, 0));
-        ptr = PREV(ptr);
-    }
-   
-    if ((trav > (char *)ptr) && (trav < NEXT(ptr))) trav = ptr; //change trav if pointing to coalesced block
-
-    return ptr;
-}
-
-/* 
- * *grow_heap - grows the heap size.
- *  - ensures alignment
- *  - adds header/footer for new free block
- *  - moves end header to the end of the grown heap
- *  - coalesces
- */
-static void *grow_heap(size_t size) 
-{
-    char *ptr = mem_sbrk(size); //set pointer to start of grown block
-    if ((long)ptr == -1)  return NULL;
-
-    /* Initialize new block's header/footer and end header */
-    PUT(HEAD(ptr), HF(ALLIGN(size)*HFSIZE, 0));
-    PUT(FOOT(ptr), HF(ALLIGN(size)*HFSIZE, 0));
-    PUT(HEAD(NEXT(ptr)), HF(0, 1)); //new end header
-
-    //return coalesce(ptr);  TO BE IMPLEMENTED
-}
-
-/* 
- * fit - Find a fit for a block with size bytes
- * - uses next fit method to search for a fit
- * - first traverses the next part of the heap (not already traversed)
- * - next, it traverses the first half
- * - if still no fit, returns NULL, if any then returns pointer to start of fit
- */
-static void *fit(size_t size)
-{
-    char *last_trav = trav; //next fit
-    /* search from the trav to the end of list */
-    for (; GET_SIZE(HEAD(trav)) > 0; trav = NEXT(trav)){
-        if (!GET_ALLOC(HEAD(trav)) && (size <= GET_SIZE(HEAD(trav)))) return trav;
-    }
-    /* search from start of list to last trav */
-    for (trav = heapL; trav < last_trav; rover = NEXT(trav)){
-        if (!GET_ALLOC(HEAD(trav)) && (size <= GET_SIZE(HEAD(trav)))) return trav;
-    }
-    return NULL; //no fit
-}
-
-/* 
- * put - Puts size byte block at the free block at ptr, splitting
- * if required.
- */
-static void put(void *ptr, size_t size)
-{
-    size_t free_size = GET_SIZE(HEAD(ptr));   
-
-    if ((free_size - size) >= (2*DWORD)) { 
-        WRITE(HEAD(ptr), HF(size, 1));
-        WRITE(FOOT(ptr), HF(size, 1));
-        ptr = NEXT(ptr);
-        WRITE(HEAD(ptr), HF(free_size-size, 0));
-        WRITE(FOOT(ptr), HF(free_size-size, 0));
-    }
-    else { 
-        WRITE(HEAD(ptr), HF(free_size, 1));
-        WRITE(FOOT(ptr), HF(free_size, 1));
-    }
-}
 //Used example in CSAPP chapter 9.9 for reference.
